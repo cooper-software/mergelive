f13057358527fa3a6db20b59fc560a39bf9a7fab
Elisha Fitch-Cook <elisha@elishacook.com>
2015-05-30 09:59:18 -0400

    Changed name to mergelive

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-diffsync
+mergelive
 ========
 
-[![Build Status](https://travis-ci.org/cooper-software/diffsync.svg?branch=master)](https://travis-ci.org/cooper-software/diffsync)
-[![Coverage Status](https://coveralls.io/repos/cooper-software/diffsync/badge.svg)](https://coveralls.io/r/cooper-software/diffsync)
+[![Build Status](https://travis-ci.org/cooper-software/mergelive.svg?branch=master)](https://travis-ci.org/cooper-software/mergelive)
+[![Coverage Status](https://coveralls.io/repos/cooper-software/mergelive/badge.svg)](https://coveralls.io/r/cooper-software/mergelive)
 
 This is a Javascript and Node implementation of [Differential Synchronization](https://neil.fraser.name/writing/sync/) for JSON documents.
---
+++ test/syncserver.spec.js
@@ -0,0 +1,80 @@
+var SyncServer = require('../lib').SyncServer
+
+var create_fake_socket = function ()
+{
+    return {
+        on: createSpy("on"),
+        send: createSpy("send")
+    }
+}
+
+describe('SyncServer', function ()
+{
+    it("throws an error if not initialized with a socket", function ()
+    {
+        var make_server_bad = function () { var server = new SyncServer({ get_object: function () {} }) }
+        expect(make_server_bad).toThrow()
+    })
+    
+    it("throws an error if not initialized with a get_object function", function ()
+    {
+        var make_server_bad = function () { var server = new SyncServer({socket:create_fake_socket()}) }
+        expect(make_server_bad).toThrow()
+    })
+    
+    it("has default event handlers", function ()
+    {
+        var server = new SyncServer({ socket: create_fake_socket(), get_object: function () {} })
+        expect(typeof(server.onchange)).toEqual("function")
+    })
+    
+    it("sets up a connection handler when initialized", function ()
+    {
+        var socket = create_fake_socket(),
+            server = new SyncServer({ socket: socket, get_object: function () {} })
+        
+        expect(socket.on).toHaveBeenCalled()
+        expect(socket.on.calls[0].args[0]).toEqual('connection')
+    })
+    
+    it("creates a handler for each new connection", function ()
+    {
+        var get_object = function () {},
+            client_socket = create_fake_socket(),
+            server_socket = create_fake_socket(),
+            server = new SyncServer({ socket: server_socket, get_object: get_object})
+        
+        server.onconnection(client_socket)
+        var handler = server.handlers[server.last_connection_id]
+        expect(handler).toBeDefined()
+        expect(handler.socket).toBe(client_socket)
+        expect(handler.get_object).toBe(get_object)
+    })
+    
+    it("deletes a handler when its connection is closed", function ()
+    {
+        var client_socket = create_fake_socket(),
+            server = new SyncServer({ socket: create_fake_socket(), get_object: function () {}})
+        
+        server.onconnection(client_socket)
+        var handler = server.handlers[server.last_connection_id]
+        expect(handler).toBeDefined()
+        expect(client_socket.on).toHaveBeenCalled()
+        var close_handler_call = client_socket.on.calls[1]
+        expect(close_handler_call.args[0]).toEqual('close')
+        expect(close_handler_call.args[1].constructor).toBe(Function)
+        close_handler_call.args[1]()
+        expect(server.handlers[server.last_connection_id]).not.toBeDefined()
+    })
+    
+    it("fires its change event and broadcasts the change when a handler has a change event", function ()
+    {
+        var get_object = function () {},
+            client_socket = create_fake_socket(),
+            server_socket = create_fake_socket(),
+            server = new SyncServer({ socket: server_socket, get_object: get_object})
+        
+        server.onconnection(client_socket)
+        
+    })
+})
